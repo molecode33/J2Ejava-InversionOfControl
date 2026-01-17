f758a8cab1c3c5fce7e6dec2ed09b14ddd671700
Momo <mobalde1298@gmail.com>
2026-01-17 10:53:42 +0000

    commite 3

--- src/main/java/metier/MetierImplementV3.java
+++ src/main/java/metier/MetierImplementV3.java
@@ -1,7 +1,10 @@
 package metier;
 
 import dao.IDao;
-
+/*
+ une nouvelle implementation de l'interface IMetier
+ On utilise juste l'injection avec le constructor
+ */
 public class MetierImplementV3 implements IMetier{
 
     private IDao dao;
